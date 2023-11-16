a30fefa3371ce6f295ea396bd70dad65aad20f1b
Anisina <makhmutovansk@gmail.com>
2023-11-16 20:38:07 +0400

    add search function

--- src/pages/Main/ui/Main.tsx
+++ src/pages/Main/ui/Main.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import image from "../../../assets/image.png";
 import style from "./Main.module.scss";
 
@@ -6,6 +7,23 @@ interface User {
   name: string;
   username: string;
   email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: {
+      lat: string;
+      lng: string;
+    };
+  };
+  phone: string;
+  website: string;
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
 }
 
 const Main = () => {
@@ -15,15 +33,96 @@ const Main = () => {
       name: "Leanne Graham",
       username: "Bret",
       email: "Sincere@april.biz",
+      address: {
+        street: "Kulas Light",
+        suite: "Apt. 556",
+        city: "Gwenborough",
+        zipcode: "92998-3874",
+        geo: {
+          lat: "-37.3159",
+          lng: "81.1496",
+        },
+      },
+      phone: "1-770-736-8031 x56442",
+      website: "hildegard.org",
+      company: {
+        name: "Romaguera-Crona",
+        catchPhrase: "Multi-layered client-server neural-net",
+        bs: "harness real-time e-markets",
+      },
     },
-
     {
       id: 2,
       name: "Ervin Howell",
       username: "Antonette",
-      email: "Sincere@april.biz",
+      email: "Shanna@melissa.tv",
+      address: {
+        street: "Victor Plains",
+        suite: "Suite 879",
+        city: "Wisokyburgh",
+        zipcode: "90566-7771",
+        geo: {
+          lat: "-43.9509",
+          lng: "-34.4618",
+        },
+      },
+      phone: "010-692-6593 x09125",
+      website: "anastasia.net",
+      company: {
+        name: "Deckow-Crist",
+        catchPhrase: "Proactive didactic contingency",
+        bs: "synergize scalable supply-chains",
+      },
     },
   ];
+
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResults, setSearchResults] = useState<User[]>([]);
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const searchTerm = event.target.value;
+    setSearchTerm(searchTerm);
+    setSearchResults(searchUsers(searchTerm));
+  };
+
+  function searchUsers(searchQuery: string): User[] {
+    const searchTerm = searchQuery.toLowerCase();
+    let filteredUsers: User[] = [];
+    const searchTermArr: string[] = searchTerm
+      .split(" ")
+      .filter((el) => el !== "");
+    if (searchTermArr.length > 1) {
+      for (let i = 0; i < searchTermArr.length; i++) {
+        const el = searchTermArr[i];
+
+        const filteredUser: User[] = users.filter((user) => {
+          return (
+            user.id.toString() === el ||
+            user.username.toLowerCase().includes(el) ||
+            user.name.toLowerCase().includes(el)
+          );
+        });
+        if (
+          filteredUsers.find((item) => filteredUser[0].name === item.name) ===
+          undefined
+        ) {
+          filteredUsers.push(filteredUser[0]);
+        }
+      }
+    } else {
+      filteredUsers = users.filter((user) => {
+        return (
+          user.id.toString() === searchTerm ||
+          user.username.toLowerCase().includes(searchTerm) ||
+          user.name.toLowerCase().includes(searchTerm)
+        );
+      });
+    }
+    if (searchTerm.length === 0) {
+      return [];
+    }
+    return filteredUsers;
+  }
 
   return (
     <div className={style.Main}>
@@ -38,11 +137,13 @@ const Main = () => {
             type="text"
             className={style.input_search}
             placeholder="Введите Id или имя"
+            value={searchTerm}
+            onChange={handleInputChange}
           />
           <div className={style.title}>Результаты</div>
           <div className={style.results_of_search}>
-            {users.length ? (
-              users.map((user) => (
+            {searchResults.length ? (
+              searchResults.map((user) => (
                 <div className={style.user_container} key={user.id}>
                   <div className={style.image}>
                     <img src={image} alt="empty_image" />
