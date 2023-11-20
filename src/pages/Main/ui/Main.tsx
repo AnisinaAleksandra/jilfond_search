14a4adbd6befd6ff1c2a94bf4e27722c217421b5
Anisina <makhmutovansk@gmail.com>
2023-11-20 13:48:44 +0400

    final + toast error notification+search

--- src/pages/Main/ui/Main.tsx
+++ src/pages/Main/ui/Main.tsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import image from "../../../assets/image.png";
 import style from "./Main.module.scss";
 import { Header } from "../../../widgets/Header";
+import { ToastError } from "../../../component/Toast/toastError";
+import { toast } from "react-toastify";
 
 interface User {
   id: number;
@@ -28,118 +30,29 @@ interface User {
 }
 
 const Main = () => {
-  const users: User[] = [
-    {
-      id: 1,
-      name: "Leanne Graham",
-      username: "Bret",
-      email: "Sincere@april.biz",
-      address: {
-        street: "Kulas Light",
-        suite: "Apt. 556",
-        city: "Gwenborough",
-        zipcode: "92998-3874",
-        geo: {
-          lat: "-37.3159",
-          lng: "81.1496",
-        },
-      },
-      phone: "1-770-736-8031 x56442",
-      website: "hildegard.org",
-      company: {
-        name: "Romaguera-Crona",
-        catchPhrase: "Multi-layered client-server neural-net",
-        bs: "harness real-time e-markets",
-      },
-    },
-    {
-      id: 2,
-      name: "Ervin Howell",
-      username: "Antonette",
-      email: "Shanna@melissa.tv",
-      address: {
-        street: "Victor Plains",
-        suite: "Suite 879",
-        city: "Wisokyburgh",
-        zipcode: "90566-7771",
-        geo: {
-          lat: "-43.9509",
-          lng: "-34.4618",
-        },
-      },
-      phone: "010-692-6593 x09125",
-      website: "anastasia.net",
-      company: {
-        name: "Deckow-Crist",
-        catchPhrase: "Proactive didactic contingency",
-        bs: "synergize scalable supply-chains",
-      },
-    },
-  ];
   const [userSelected, setUserSelected] = useState<User | null>(null);
   const [searchTerm, setSearchTerm] = useState("");
   const [searchResults, setSearchResults] = useState<User[]>([]);
-  console.log(userSelected);
+  const [listOfUsers, setListOfUsers] = useState<User[]>([]);
+  const [loader, setLoader] = useState(false);
 
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const searchTerm = event.target.value;
-    setSearchTerm(searchTerm);
-    setSearchResults(searchUsers(searchTerm));
-  };
-  const selectHandle = (user: User) => {
-    setUserSelected(user);
-  };
-  function searchUsers(searchQuery: string): User[] {
-    const searchTerm = searchQuery.toLowerCase();
-    let filteredUsers: User[] = [];
-    const searchTermArr: string[] = searchTerm
-      .split(" ")
-      .filter((el) => el !== "");
-    if (searchTermArr.length > 1) {
-      for (let i = 0; i < searchTermArr.length; i++) {
-        const el = searchTermArr[i];
-        const filteredUser: User[] = users.filter((user) => {
-          return (
-            user.id.toString() === el ||
-            user.username.toLowerCase().includes(el) ||
-            user.name.toLowerCase().includes(el)
-          );
-        });
-        if (
-          filteredUsers.find((item) => filteredUser[0].name === item.name) ===
-          undefined
-        ) {
-          filteredUsers.push(filteredUser[0]);
-        }
-      }
-    } else {
-      filteredUsers = users.filter((user) => {
-        return (
-          user.id.toString() === searchTerm ||
-          user.username.toLowerCase().includes(searchTerm) ||
-          user.name.toLowerCase().includes(searchTerm)
-        );
-      });
-    }
-    if (searchTerm.length === 0) {
-      return [];
-    }
-    return filteredUsers;
-  }
-
-  const userIds = [1, 2]; // IDs for the query parameter
+  const textOrLoader = loader ? (
+    <div className={style.lds_ellipsis}>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  ) : (
+    <div>начните поиск </div>
+  );
 
   useEffect(() => {
     const fetchUsers = async () => {
       try {
         const params = new URLSearchParams();
-        if (searchTerm !== "") {
-          userIds.forEach((id) => params.append("id", String(id)));
-        }
-        const url: string =
-          searchTerm === ""
-            ? `https://jsonplaceholder.typicode.com/users`
-            : `https://jsonplaceholder.typicode.com/users?${params.toString()}`;
+        searchResults.forEach((el) => params.append("id", String(el.id)));
+        const url: string = `https://jsonplaceholder.typicode.com/users?${params.toString()}`;
         const response = await fetch(url, {
           method: "GET",
         });
@@ -149,49 +62,110 @@ const Main = () => {
         }
 
         const data = await response.json();
-        setSearchResults(data);
+        if (listOfUsers.length > data.length) {
+          setSearchResults(data);
+        } else if (listOfUsers.length < data.length) {
+          setListOfUsers(data);
+        }
       } catch (error) {
         console.error("Error fetching users:", error);
+        toast.error("Error!", {
+          position: "top-center",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+        });
       }
     };
-
     fetchUsers();
+    if (searchTerm.length === 0) {
+      setUserSelected(null);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [searchTerm]);
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const searchTerm = event.target.value;
+    setSearchTerm(searchTerm);
+    const searchTermArr: string[] = searchTerm
+      .split(" ")
+      .filter((el) => el !== "");
+    setLoader(true);
+    setTimeout(() => {
+      setSearchResults(searchUsers(searchTermArr));
+      setLoader(false);
+    }, 2000);
+  };
+
+  function searchUsers(searchQueryArr: string[]): User[] {
+    const searchTermArr: string[] = searchQueryArr.map((el: string) =>
+      el.toLowerCase()
+    );
+    const filteredUsers: User[] = [];
+    if (searchTermArr.length >= 1) {
+      for (let i = 0; i < searchTermArr.length; i++) {
+        const el = searchTermArr[i];
+        const filteredUser: User[] = listOfUsers.filter((user) => {
+          return (
+            user.id.toString() === el ||
+            user.username.toLowerCase().includes(el) ||
+            user.name.toLowerCase().includes(el)
+          );
+        });
+        if (
+          filteredUsers!.find((item) => filteredUser[0]!.name === item.name) ===
+          undefined
+        ) {
+          filteredUsers.push(filteredUser[0]);
+        }
+      }
+    }
+    return filteredUsers;
+  }
+  const selectHandle = (user: User) => {
+    setUserSelected(user);
+  };
 
   return (
     <div className={style.Main}>
       <Header />
+      <ToastError />
       <div className={style.page_main}>
         <div className={style.side_bar}>
-          <div className={style.title}>Поиск сотрудников</div>
-          <input
-            type="text"
-            className={style.input_search}
-            placeholder="Введите Id или имя"
-            value={searchTerm}
-            onChange={handleInputChange}
-          />
-          <div className={style.title}>Результаты</div>
+          <div className={style.top_part}>
+            <div className={style.title}>Поиск сотрудников</div>
+            <input
+              type="text"
+              className={style.input_search}
+              placeholder="Введите Id или имя"
+              value={searchTerm}
+              onChange={handleInputChange}
+            />
+            <div className={style.title}>Результаты</div>
+          </div>
+
           <div className={style.results_of_search}>
-            {searchResults.length ? (
-              searchResults.map((user) => (
-                <div
-                  className={style.user_container}
-                  key={user.id}
-                  onClick={() => selectHandle(user)}
-                >
-                  <div className={style.image}>
-                    <img src={image} alt="empty_image" />
+            {searchResults.length
+              ? searchResults.map((user) => (
+                  <div
+                    className={style.user_container}
+                    key={user.id}
+                    onClick={() => selectHandle(user)}
+                  >
+                    <div className={style.image}>
+                      <img src={image} alt="empty_image" />
+                    </div>
+                    <div className={style.info_user}>
+                      <div>{user.name}</div>
+                      <div>{user.email}</div>
+                    </div>
                   </div>
-                  <div className={style.info_user}>
-                    <div>{user.name}</div>
-                    <div>{user.email}</div>
-                  </div>
-                </div>
-              ))
-            ) : (
-              <div>начните поиск </div>
-            )}
+                ))
+              : textOrLoader}
           </div>
         </div>
         <div className={style.main_part}>
@@ -201,16 +175,16 @@ const Main = () => {
                 <img src={image} alt="empty_image" />
               </div>
               <div className={style.user_info}>
-                <div className={style.field_info}>{userSelected.name}</div>
+                <div className={style.field_info}>{userSelected!.name}</div>
                 <div className={style.field_info}>
-                  email: <span> {userSelected.email}</span>
+                  email: <span> {userSelected!.email}</span>
                 </div>
                 <div className={style.field_info}>
-                  phone: <span> {userSelected.phone}</span>
+                  phone: <span> {userSelected!.phone}</span>
                 </div>
                 <div className={style.field_info_title}>О себе</div>
                 <div className={style.field_info}>
-                  {userSelected.company.catchPhrase}
+                  {userSelected!.company.catchPhrase}
                 </div>
               </div>
             </>
